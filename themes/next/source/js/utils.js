fa56c4d12ac4afb0e4fb6eb3add46d645d1cef54
ziteh <honmonoh@gmail.com>
2021-11-17 22:00:34 +0800

    修復文章目錄無法跳轉的問題

--- themes/next/source/js/utils.js
+++ themes/next/source/js/utils.js
@@ -236,7 +236,8 @@ NexT.utils = {
       // TOC item animation navigate.
       link.addEventListener('click', event => {
         event.preventDefault();
-        var target = document.getElementById(event.currentTarget.getAttribute('href').replace('#', ''));
+        // Ref: https://github.com/theme-next/hexo-theme-next/issues/1543#issuecomment-668549582
+        var target = document.getElementById(decodeURI(event.currentTarget.getAttribute('href').replace('#', '')));
         var offset = target.getBoundingClientRect().top + window.scrollY;
         window.anime({
           targets  : document.scrollingElement,
@@ -245,7 +246,7 @@ NexT.utils = {
           scrollTop: offset + 10
         });
       });
-      return document.getElementById(link.getAttribute('href').replace('#', ''));
+      return document.getElementById(decodeURI(link.getAttribute('href').replace('#', '')));
     });
 
     var tocElement = document.querySelector('.post-toc-wrap');
