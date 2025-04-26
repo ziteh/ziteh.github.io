f1ff0998d6828f29d647864d3f05f6c23d6d968e
ZiTe <honmonoh@gmail.com>
2025-04-26 12:46:38 +0800

    feat: add ban google bot env

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,7 +1,14 @@
 import type { APIRoute } from "astro";
 import { SITE } from "@config";
 
+const banGooglebot = `
+User-agent: Googlebot
+Disallow: /
+
+`;
+
 const robots = `
+${import.meta.env.PUBLIC_BAN_GOOGLEBOT === "true" ? banGooglebot : ""}
 User-agent: *
 Allow: /
 
