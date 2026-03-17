6db22548a1e3f3947378d54cb60c0f0ac592e5e8
ZiTe <honmonoh@gmail.com>
2026-03-17 23:15:04 +0800

    feat: upgrade Astro from v5 to v6

--- astro.config.ts
+++ astro.config.ts
@@ -167,10 +167,6 @@ export default defineConfig({
   image: {
     // Used for all Markdown images; not configurable per-image
     // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
-    experimentalLayout: "constrained",
-  },
-  experimental: {
-    responsiveImages: true,
-    preserveScriptOrder: true,
+    layout: "constrained",
   },
 });
